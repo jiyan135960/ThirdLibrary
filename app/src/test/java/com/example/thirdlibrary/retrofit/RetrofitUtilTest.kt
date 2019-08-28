de2d2933dda30c57ef9b9bb2bcf79669e9a80ad2
zhangyuxiang <zyx@gs-robot.com>
2019-08-28 20:03:55 +0800

    添加retrofit mock

--- app/src/test/java/com/example/thirdlibrary/retrofit/RetrofitUtilTest.kt
+++ app/src/test/java/com/example/thirdlibrary/retrofit/RetrofitUtilTest.kt
@@ -1,14 +1,14 @@
 package com.example.thirdlibrary.retrofit
 
+import com.example.thirdlibrary.util.RetrofitUtil
 import org.junit.Test
 
 class RetrofitUtilTest {
 
     @Test
     fun testRetrofitUtilInit() {
-//        val testService: TestService = RetrofitUtil.createService(
-//            TestService::class)
-//        val recommendList = testService.musicRecommendList()
+        val testService: TestService = RetrofitUtil.createService(TestService::class)
+        val recommendList = testService.musicRecommendList()
     }
 
 }
---
+++ app/src/test/java/com/example/thirdlibrary/retrofit/mock/MockTestService.kt
@@ -0,0 +1,6 @@
+package com.example.thirdlibrary.retrofit.mock
+
+/**
+ * Created by zyx on 19-8-28.
+ */
+class MockTestService : Mock
